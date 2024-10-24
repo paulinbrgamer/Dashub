837552d0ad132c9e278dc4f11332001fe7d84bd1
Paulo <pauloferadosgames@gmail.com>
2024-10-24 10:31:32 -0300

    implementando url no cadastro

--- Front-end/cadastro/cadastro.js
+++ Front-end/cadastro/cadastro.js
@@ -1,25 +1,17 @@
+var token;
+var url = 'https://api-dashub-dev.up.railway.app/'
 async function cadastrar(){
-    var img = document.getElementById('load')
-    img.classList.add('mostrar')
-    img.classList.remove('apagar')
     var name = document.getElementById('idnome').value
-    var date = document.getElementById('iddate').value
     var mail = document.getElementById('idemail').value
     var password = document.getElementById('ipword').value
-    
-    var requisicao = await fetch('https://api-to-do-a5kr.onrender.com/register',{
+    var requisicao = await fetch(url+'auth/register',{
         method:'POST',
         headers: {
             'Content-Type': 'application/json', // Define o tipo do conteúdo
         },
-        body: JSON.stringify({nome:name,email:mail,data_nascimento:date,senha:password})
+        body: JSON.stringify({nome:name,email:mail,senha:password})
     })
-    img.classList.remove('mostrar')
-    img.classList.add('apagar')
     if(requisicao.ok){
-        var link = document.createElement('a')
-        link.href = 'index.html'
-        link.click()
     }
     else{
         var b= await requisicao.json()
