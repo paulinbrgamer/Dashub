06e527c53faeb13e1e5dfc393fc1f402001def81
Paulo <pauloferadosgames@gmail.com>
2024-10-25 11:57:08 -0300

    implementado o leitor de csv

--- Front-end/home/home.js
+++ Front-end/home/home.js
@@ -1023,6 +1023,12 @@ function criarGraficoPainel(){
             </div>
                         
             <h1 class="item">Dados</h1>
+            <div>
+            <div  id="container-arquivo">
+                <label  id="label-input" for="dados">Carregar Dados CSV</label> <input  type="file" name="arquivo" id="dados" >
+                
+                
+            </div></div>
             <div id="container-data">            
             </div>
             <button onclick="user.novoGrafico()" id="criar-g" style="display:none;background-color: #0AC00A;width: 60%;padding: 10px;border-radius: 6px;align-self: center;">Criar Gráfico</button>
@@ -1030,12 +1036,141 @@ function criarGraficoPainel(){
     as.botaoAmostra="dashs"
     as.clicks--
     document.querySelector('#container-home').innerHTML = telanovo
-    
+    leituradosdados()
 }
 function AbrirRepositorios(){
     var home = document.createElement('a')
         home.href = 'repositorios.html'
         home.click()
+}
+//função que pega os dados em forma de texto e faz a analise lexa para dividir os dados e guardar cada "celula" em um array
+
+function leituradosdados(){
+    //variavel para guardar arquivo selecionado
+const input = document.querySelector('#dados')
+//variavel string para guardar os dados em forma de texto do arquivo csv
+var dados;
+//variavel com todas aslinhas guardadas em arrays diferentes
+var array_dados = [];
+var nome_arquivo;
+//array que contem o id de todas as cells
+var array_cells = []
+//botao de salvar
+var salvar = document.getElementById('save')
+var numero_de_colunas = 0
+var numero_de_linhas = 0
+
+//acionar evento quando o estado do leitor de arquivo mudar
+input.addEventListener('change',function(){
+        
+    //selecionar o primeiro arquivo e guardar na variavel
+    const arquivos = this.files[0]
+    nome_arquivo = this.files[0].name
+    //instanciar um leitor para esse arquivo
+    const reader = new FileReader()
+
+    //adicionar um evento ao leitor para quando o arquivo for carregado
+    reader.addEventListener('load',function(){
+        
+        //guardar a leitura desse arquivo na variavel string
+        dados = reader.result
+        //chamar função e passar a variavel string como parametro para que os dados retornem como um array
+        array_dados = Csv_reader(dados)
+        
+        console.log(array_dados)
+        
+    })
+    //ler os dados como texto
+    reader.readAsText(arquivos)
+})
+//função que pega os dados em forma de texto e faz a analise lexa para dividir os dados e guardar cada "celula" em um array
+    function Csv_reader(data){
+    //array que contera todas as celulas
+    var linhas = []
+    var linhas_totais = []
+    //variavel que marca o inicio do dado
+    var inicio_palavra= 0
+    var string = '';
+    var primeira_linha = true
+    //laço for para percorrer todos os caracteres da string data que foi passada no parametro da função
+    for (var fim_palavra = 0; fim_palavra<= data.length;fim_palavra++){
+        //condição para encontrar as quebras de linhas que ficam no arquivo CSV e nao lelas como dados
+        if (data[fim_palavra] === '\r'){
+
+            //laço for que percorre a string até a virgula e guarda ela na variavel string
+            for (inicio_palavra; inicio_palavra<fim_palavra;inicio_palavra++){
+                
+                string = string+data[inicio_palavra]
+            }
+            //guardando a variavel string em um array, linpando a variavel string e selecionando o final do dado como inicio do proximo e colocando em um array separado por linhas
+            if(string.length<1){
+                string = ' '
+            }
+            linhas.push(string)
+            linhas_totais.push(linhas)
+            linhas = []
+            string = ''
+            //pular o \r e o \n por isso numero é dois
+            inicio_palavra+= 2
+            primeira_linha = false
+            numero_de_linhas++
+        }
+        //condição para encontrar a virgula que separa os dados
+         if (data[fim_palavra] == ',' || data[fim_palavra] == ';'){
+
+            //laço for que percorre a string até a virgula e guarda ela na variavel string
+            for (inicio_palavra; inicio_palavra<fim_palavra ;inicio_palavra++){
+                
+                string = string+data[inicio_palavra]
+            }
+            if(string.length<1){
+                string = ' '
+            }
+            
+            //guardando a variavel string em um array, linpando a variavel string e selecionando o final do dado como inicio do proximo
+            linhas.push(string)
+            string = ''
+            inicio_palavra++
+            if (primeira_linha == true){
+                numero_de_colunas++
+                
+            }
+        }
+        //condição que identifica se está na ultima lina e guarda o ultimo valor por conta de nao haver virgula
+        if (typeof dados[fim_palavra] == "undefined"){
+            for (inicio_palavra; inicio_palavra<fim_palavra ;inicio_palavra++){
+                string = string+data[inicio_palavra]
+                
+
+            }
+            //guardando a variavel string em um array, linpando a variavel string e selecionando o final do dado como inicio do proximo e colocando em um array separado por linhas
+            if(string.length<1){
+                string = ''
+            }
+            linhas.push(string)
+            linhas_totais.push(linhas)
+            linhas = []
+            string = ''
+            inicio_palavra++
+            numero_de_colunas++
+            numero_de_linhas++
+        }
+        
+        
+        
+   }
+   
+   linhas_totais.forEach(function(linha){
+    while (linhas_totais[0].length > linha.length){
+        linha.push("")
+        
+        
+        
+    }
+        
+   })
+    return linhas_totais
+    }
 }
 
 //conectar com o backend
