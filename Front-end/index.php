6adba33fa1ca237d995910ec4bb2bd8e072e4e71
oPaulin <pauloferadosgames@gmail.com>
2024-11-20 19:19:35 -0300

    codigo integrado direto no php do login

--- Front-end/index.php
+++ Front-end/index.php
@@ -1,4 +1,6 @@
-<? ?>
+<? 
+
+?>
 
 
 
@@ -35,6 +37,48 @@
         <a style="width: 70px;" href="cadastro.html">Registrar</a>
     </form>
 
-    <script src="login/login.js"></script>
+    <script >
+        async function login(){
+        var img = document.getElementById('load')
+        img.classList.add('mostrar')
+        img.classList.remove('apagar')
+        var email = document.getElementById('idemail').value
+        var pass = document.getElementById('ipword').value
+        var res = await fetch(url+'auth/login',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json', // Define o tipo do conteúdo
+            },
+            body:JSON.stringify({
+                    email: email,
+                    senha: pass
+            })  
+        })
+        img.classList.remove('mostrar')
+        img.classList.add('apagar')
+        if(!res.ok){
+            
+            var body = await res.json()
+            if (body.msg){
+                window.alert(body.msg)
+            }
+        }
+        else{
+            var body = await res.json()    
+            sessionStorage.setItem('email',body.email)
+            sessionStorage.setItem('nome',body.nome)
+            sessionStorage.setItem('token',body.token)
+            var home = document.createElement('a')
+            home.href = 'home.html'
+            home.click()
+            
+        }
+    
+    
+        }
+
+
+
+    </script>
 </body>
 </html>
